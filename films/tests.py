0238afda3b8ddfbb7a866b8ce463ddcef2dc4885
Victor Garcia <victor.g64r@gmail.com>
2020-09-25 01:53:14 -0500

    Upgrade a film unit test

--- films/tests.py
+++ films/tests.py
@@ -37,7 +37,7 @@ class DirectorTestCase(TestCase):
 class FilmTestCase(TestCase):
     def setup(self):
         saoirse = Actor.objects.create(name='Saoirse', last_name='Ronan', birthdate=date(1994 , 4 , 12), country='USA')
-        film_instance = Film.objects.create(title='Lady Bird', duration='94 minutes', genre='Drama', price=50.00)
+        film_instance = Film.objects.create(title='Lady Bird', duration='94 minutes', year=2017, genre='Drama', price=50.00)
         film_instance.actors.add(saoirse)
     
     def test_get_movies(self):
