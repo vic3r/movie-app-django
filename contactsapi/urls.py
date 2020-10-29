07c5a76883591105a6b1d2d57c3c01be5e79cadc
Victor Garcia <victor.g64r@gmail.com>
2020-10-28 23:38:24 -0600

    Remove repeated path

--- contactsapi/urls.py
+++ contactsapi/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('films/', include('films.urls')),
+    path('', include('films.urls')),
     path('auth/', include('authentication.urls')),
 ]
